e970a42493dc143cc01d86e40611ce0d8b13d8cc
Chris Fraser <cfraz89@gmail.com>
2018-06-02 02:30:33 +1000

    Make new-show-build-info functional

--- cabal-install/Distribution/Client/CmdShowBuildInfo.hs
+++ cabal-install/Distribution/Client/CmdShowBuildInfo.hs
@@ -3,16 +3,10 @@
 module Distribution.Client.CmdShowBuildInfo (
     -- * The @build@ CLI and action
     showBuildInfoCommand,
-    showBuildInfoAction,
-
-    -- * Internals exposed for testing
-    TargetProblem(..),
-    selectPackageTargets,
-    selectComponentTarget
+    showBuildInfoAction
   ) where
 
 import Distribution.Client.ProjectOrchestration
-import Distribution.Client.CmdErrorMessages
 
 import Distribution.Client.Setup
          ( GlobalFlags, ConfigFlags(..), ConfigExFlags, InstallFlags )
@@ -24,7 +18,7 @@ import Distribution.Simple.Command
 import Distribution.Verbosity
          ( Verbosity, normal )
 import Distribution.Simple.Utils
-         ( wrapText, die' )
+         ( wrapText)
 
 import qualified Data.Map as Map
 import qualified Distribution.Simple.Setup as Cabal
@@ -32,9 +26,12 @@ import Distribution.Client.SetupWrapper
 import Distribution.Simple.Program ( defaultProgramDb )
 import qualified Distribution.Client.InstallPlan as InstallPlan
 import Distribution.Client.ProjectPlanning.Types
-import Distribution.Client.ProjectPlanning (setupHsBuildFlags)
+import Distribution.Client.ProjectPlanning (setupHsBuildFlags, setupHsScriptOptions)
 import Distribution.Client.DistDirLayout (distBuildDirectory)
-
+import Distribution.Types.PackageName (mkPackageName)
+import Distribution.Types.PackageId (pkgName)
+import Distribution.Client.Types ( PackageLocation(..), GenericReadyPackage(..) )
+import Distribution.Client.JobControl (newLock, Lock)
 
 showBuildInfoCommand :: CommandUI (ConfigFlags, ConfigExFlags, InstallFlags, HaddockFlags)
 showBuildInfoCommand = Client.installCommand {
@@ -82,130 +79,37 @@ showBuildInfoAction :: (ConfigFlags, ConfigExFlags, InstallFlags, HaddockFlags)
 showBuildInfoAction (configFlags, configExFlags, installFlags, haddockFlags)
             targetStrings globalFlags = do
 
-    baseCtx <- establishProjectBaseContext verbosity cliConfig
+  baseCtx <- establishProjectBaseContext verbosity cliConfig
 
-    targetSelectors <- either (reportTargetSelectorProblems verbosity) return
-                   =<< readTargetSelectors (localPackages baseCtx) targetStrings
+  buildCtx <-
+    runProjectPreBuildPhase verbosity baseCtx $ \elaboratedPlan ->
+          return (elaboratedPlan, Map.empty)
 
-    buildCtx <-
-      runProjectPreBuildPhase verbosity baseCtx $ \elaboratedPlan -> do
+  let baseCtx' = baseCtx {
+                    buildSettings = (buildSettings baseCtx) {
+                      buildSettingDryRun = True
+                    }
+                  }
 
-            -- Interpret the targets on the command line as build targets
-            -- (as opposed to say repl or haddock targets).
-            targets <- either (reportTargetProblems verbosity) return
-                     $ resolveTargets
-                         selectPackageTargets
-                         selectComponentTarget
-                         TargetProblemCommon
-                         elaboratedPlan
-                         targetSelectors
-
-            let elaboratedPlan' = pruneInstallPlanToTargets
-                                    TargetActionBuild
-                                    targets
-                                    elaboratedPlan
-            elaboratedPlan'' <-
-              if buildSettingOnlyDeps (buildSettings baseCtx)
-                then either (reportCannotPruneDependencies verbosity) return $
-                     pruneInstallPlanToDependencies (Map.keysSet targets)
-                                                    elaboratedPlan'
-                else return elaboratedPlan'
-
-            return (elaboratedPlan'', targets)
-
-    printPlan verbosity baseCtx buildCtx
-    -- buildOutcomes <- runProjectBuildPhase verbosity baseCtx buildCtx
-    -- runProjectPostBuildPhase verbosity baseCtx buildCtx buildOutcomes
-    -- plan rpkg
-    -- rpkg@(ReadyPackage pkg)
-    -- let dsc = elabPkgDescription pkg
-    -- print $ pkg buildCtx
-    let pkg = (last . pkgList) buildCtx
-        shared = elaboratedShared buildCtx
-        builddir = distBuildDirectory (distDirLayout baseCtx) (elabDistDirParams shared pkg)
-        flags = setupHsBuildFlags pkg shared verbosity builddir
-    setupWrapper verbosity defaultSetupScriptOptions (Just $ elabPkgDescription pkg) (Cabal.showBuildInfoCommand defaultProgramDb) (const flags)  []
-
+  scriptLock <- newLock
+  mapM_ (showInfo verbosity baseCtx' buildCtx scriptLock (configured buildCtx)) targetStrings
+  
   where
     verbosity = fromFlagOrDefault normal (configVerbosity configFlags)
     cliConfig = commandLineFlagsToProjectConfig
                   globalFlags configFlags configExFlags
                   installFlags haddockFlags
-    pkgList ctx = [s | InstallPlan.Installed s <- InstallPlan.toList (elaboratedPlanOriginal ctx)]
-
--- | This defines what a 'TargetSelector' means for the @bench@ command.
--- It selects the 'AvailableTarget's that the 'TargetSelector' refers to,
--- or otherwise classifies the problem.
---
--- For the @build@ command select all components except non-buildable and disabled
--- tests\/benchmarks, fail if there are no such components
---
-selectPackageTargets :: TargetSelector
-                     -> [AvailableTarget k] -> Either TargetProblem [k]
-selectPackageTargets targetSelector targets
-
-    -- If there are any buildable targets then we select those
-  | not (null targetsBuildable)
-  = Right targetsBuildable
-
-    -- If there are targets but none are buildable then we report those
-  | not (null targets)
-  = Left (TargetProblemNoneEnabled targetSelector targets')
-
-    -- If there are no targets at all then we report that
-  | otherwise
-  = Left (TargetProblemNoTargets targetSelector)
-  where
-    targets'         = forgetTargetsDetail targets
-    targetsBuildable = selectBuildableTargetsWith
-                         (buildable targetSelector)
-                         targets
-
-    -- When there's a target filter like "pkg:tests" then we do select tests,
-    -- but if it's just a target like "pkg" then we don't build tests unless
-    -- they are requested by default (i.e. by using --enable-tests)
-    buildable (TargetPackage _ _  Nothing) TargetNotRequestedByDefault = False
-    buildable (TargetAllPackages  Nothing) TargetNotRequestedByDefault = False
-    buildable _ _ = True
-
--- | For a 'TargetComponent' 'TargetSelector', check if the component can be
--- selected.
---
--- For the @build@ command we just need the basic checks on being buildable etc.
---
-selectComponentTarget :: SubComponentTarget
-                      -> AvailableTarget k -> Either TargetProblem k
-selectComponentTarget subtarget =
-    either (Left . TargetProblemCommon) Right
-  . selectComponentTargetBasic subtarget
+    configured ctx = [p | InstallPlan.Configured p <- InstallPlan.toList (elaboratedPlanOriginal ctx)]
 
 
--- | The various error conditions that can occur when matching a
--- 'TargetSelector' against 'AvailableTarget's for the @build@ command.
---
-data TargetProblem =
-     TargetProblemCommon       TargetProblemCommon
-
-     -- | The 'TargetSelector' matches targets but none are buildable
-   | TargetProblemNoneEnabled TargetSelector [AvailableTarget ()]
-
-     -- | There are no targets at all
-   | TargetProblemNoTargets   TargetSelector
-  deriving (Eq, Show)
-
-reportTargetProblems :: Verbosity -> [TargetProblem] -> IO a
-reportTargetProblems verbosity =
-    die' verbosity . unlines . map renderTargetProblem
-
-renderTargetProblem :: TargetProblem -> String
-renderTargetProblem (TargetProblemCommon problem) =
-    renderTargetProblemCommon "build" problem
-renderTargetProblem (TargetProblemNoneEnabled targetSelector targets) =
-    renderTargetProblemNoneEnabled "build" targetSelector targets
-renderTargetProblem(TargetProblemNoTargets targetSelector) =
-    renderTargetProblemNoTargets "build" targetSelector
-
-reportCannotPruneDependencies :: Verbosity -> CannotPruneDependencies -> IO a
-reportCannotPruneDependencies verbosity =
-    die' verbosity . renderCannotPruneDependencies
-
+showInfo :: Verbosity -> ProjectBaseContext -> ProjectBuildContext -> Lock -> [ElaboratedConfiguredPackage] -> String -> IO ()
+showInfo verbosity baseCtx buildCtx lock pkgs targetName = setupWrapper verbosity scriptOptions (Just $ elabPkgDescription pkg) (Cabal.showBuildInfoCommand defaultProgramDb) (const flags)  []
+  where pkg = head $ filter isTarget pkgs
+        isTarget = (mkPackageName targetName ==) . pkgName . elabPkgSourceId
+        shared = elaboratedShared buildCtx
+        buildDir = distBuildDirectory (distDirLayout baseCtx) (elabDistDirParams shared pkg)
+        flags = setupHsBuildFlags pkg shared verbosity buildDir
+        srcDir = case (elabPkgSourceLocation pkg) of
+          LocalUnpackedPackage fp -> fp
+          _ -> ""
+        scriptOptions = setupHsScriptOptions (ReadyPackage pkg) shared srcDir buildDir False lock
