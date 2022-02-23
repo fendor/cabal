3c977f8746ffa9f3cdb66163e8eb1382cf10ed34
Fendor <power.walross@gmail.com>
2022-02-23 19:00:50 +0100

    WIP

--- cabal-testsuite/PackageTests/Ambiguity/setup-cabal-file.test.hs
+++ cabal-testsuite/PackageTests/Ambiguity/setup-cabal-file.test.hs
@@ -1,5 +1,8 @@
 import Test.Cabal.Prelude
 -- Test that we can resolve a module name ambiguity when reexporting
 -- by explicitly specifying what package we want.
-main = cabalTest $ withDirectory "multiple-cabal-files" $
-    fails $ cabal "v2-build" []
+main = cabalTest $ do
+    withDirectory "multiple-cabal-files" $
+        fails $ cabal "v2-build" []
+    -- withDirectory "multiple-cabal-files-project" $
+    --     fails $ cabal "v2-build" ["all"]
